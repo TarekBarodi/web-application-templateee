b095b8e99132eeb25df48c941ba9b523dd984799
Abdul Hameed <abdnaqip1980@gmail.com>
2019-11-21 14:57:59 +0100

    Autowired the PostRepository and used it to implement all the post service methods

--- src/main/java/se/kth/sda6/skeleton/posts/PostService.java
+++ src/main/java/se/kth/sda6/skeleton/posts/PostService.java
@@ -1,34 +1,59 @@
 package se.kth.sda6.skeleton.posts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /*
     @TODO Autowire the PostRepository and use it to implement all the service methods.
  */
+@Service
 public class PostService {
-    public List<Post> getAll() {
-        // @TODO get all posts and return them as List<Post>
-        return null;
+    //public static List<Post> posts = new ArrayList<>();
+    @Autowired
+    private PostRepository postRepository;
+
+    //get all posts and return them as List<Post>
+    public List<Post> getAll(String sort) {
+        if (sort.equals("body")){
+            //return posts.stream().sorted(Comparator.comparing(Post::getBody)).collect(Collectors.toList());
+            return postRepository.findAll().stream()
+                    .sorted(Comparator.comparing(Post::getBody))
+                    .collect(Collectors.toList());
+        } else {
+            //return posts;
+            return postRepository.findAll();
+        }
     }
 
+    //get a post by ID if it exists
     public Optional<Post> getByID(Long id) {
-        // @TODO get a post by ID if it exists
-        return null;
+        //return posts.stream().filter(t -> t.getId().equals(id)).findFirst();
+        return postRepository.findById(id);
     }
 
+    //save the post to DB and return the saved post
     public Post save(Post post) {
-        // @TODO save the post to DB and return the saved post
-        return null;
+        //posts.add(post);
+        postRepository.save(post);
+        return post;
     }
 
+    //update a post
     public Optional<Post> update(Post post) {
         // @TODO update the post if it exists in DB and return the updated post.
-        return null;
+        postRepository.save(post);
+        return postRepository.findById(post.getId());
     }
 
+    //delete the post by id
     public void deleteById(Long id) {
-        // @TODO delete the post by id
-        return;
+        //posts = posts.stream().filter(t -> !t.getId().equals(id)).collect(Collectors.toList());
+        postRepository.deleteById(id);
     }
 }
