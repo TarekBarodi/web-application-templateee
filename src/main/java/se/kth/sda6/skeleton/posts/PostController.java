2f6a88a33b6db3495d4383a3d4a44f4c69c65fd5
Abdul Hameed <abdnaqip1980@gmail.com>
2019-11-21 15:00:58 +0100

    AutoWired PostService, created the methods calling the post service methods, used the spring notations. These methods needed to implement the API

--- src/main/java/se/kth/sda6/skeleton/posts/PostController.java
+++ src/main/java/se/kth/sda6/skeleton/posts/PostController.java
@@ -1,10 +1,53 @@
 package se.kth.sda6.skeleton.posts;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
     @TODO AutoWire PostService and create the methods needed to implement the API.
     Don't forget to add necessary annotations.
  */
+@RestController
+@RequestMapping("/posts")
 public class PostController {
+    @Autowired
+    public PostService postService;
+
+
+    //get the list of posts
+    @GetMapping("")
+    public List<Post> getAll(@RequestParam(required = false) String sort){
+        return postService.getAll(sort);
+    }
+
+    //get a post by id
+    @GetMapping("/{id}")
+    public Post getByID(@PathVariable Long id){
+        return postService.getByID(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+
+    //create post
+    @PostMapping("")
+    public Post save(@RequestBody Post post) {
+        return postService.save(post);
+    }
+
+    //update a post
+    @PutMapping("")
+    public Post update(@RequestBody Post post){
+        return postService.update(post)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        postService.deleteById(id);
+    }
 }
