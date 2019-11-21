93edb7cee8d885c6de1f2d4c96db3a188155a796
Abdul Hameed <abdnaqip1980@gmail.com>
2019-11-21 14:54:42 +0100

    add Hibernate annotations to define which table and columns should be used to save the Post Object

--- src/main/java/se/kth/sda6/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda6/skeleton/posts/Post.java
@@ -1,15 +1,30 @@
 package se.kth.sda6.skeleton.posts;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+
 // @TODO add Hibernate annotations to define which table and columns should be used to save the Post Object.
+@Entity
+@Table (name = "post")
 public class Post {
+
+    @Id
+    @Column (name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column (name = "body")
     private String body;
 
 //    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
 //    private List<Comment> comments = new ArrayList<>();
 
     public Post() {
+    }
+
+    public Post(Long id, String body) {
+        this.id = id;
+        this.body = body;
     }
 
     public Post(String body) {
